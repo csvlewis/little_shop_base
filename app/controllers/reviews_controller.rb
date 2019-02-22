e82d2bc20f3f4149aebcb7057cb1b2177d0d3a56
Chris Lewis <13chlewis@gmail.com>
2019-02-22 16:06:58 -0700

    Create action for Review controller complete

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,7 @@ class ReviewsController < ApplicationController
   before_action :require_user
 
   def index
+
   end
 
   def new
@@ -10,6 +11,18 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @item =  OrderItem.find(params[:order_item_id]).item
+    @review = Review.create(review_params)
+    @review.order_item_id = params[:order_item_id]
+    @review.username = current_user.name
+    @review.item_name = @item.name
+    current_user.reviews << @review
     redirect_to user_reviews_path(current_user)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :description, :rating)
   end
 end
