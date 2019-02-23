efd4008e3b58c7056efa4ebaa0f3b69dd08bb096
Chris Lewis <13chlewis@gmail.com>
2019-02-23 10:42:29 -0700

    Updated database and wrote model tests for Address model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,14 +5,12 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
     it { should validate_presence_of :name }
-    it { should validate_presence_of :address }
-    it { should validate_presence_of :city }
-    it { should validate_presence_of :state }
-    it { should validate_presence_of :zip }
+
   end
 
   describe 'relationships' do
     # as user
+    it { should have_many :addresses }
     it { should have_many :orders }
     it { should have_many(:order_items).through(:orders)}
     # as merchant
